393b28847c4f98cb7347a3cf500f42a160de3a37
deviousprophet <69911034+deviousprophet@users.noreply.github.com>
2023-07-06 23:29:40 +0700

    idf v5.1 update

--- esp_stmems_lsm9ds1.c
+++ esp_stmems_lsm9ds1.c
@@ -4,13 +4,13 @@
 
 static const char* TAG = "lsm9ds1";
 
-esp_err_t lsm9ds1_read_reg(stmdev_ctx_t* ctx, uint8_t reg, uint8_t* data, uint16_t len) {
+int32_t lsm9ds1_read_reg(stmdev_ctx_t* ctx, uint8_t reg, uint8_t* data, uint16_t len) {
     esp_err_t ret = ctx->read_reg(ctx->handle, reg, data, len);
     ESP_RETURN_ON_FALSE(ret == ESP_OK, ret, TAG, "read reg failed");
     return ESP_OK;
 }
 
-esp_err_t lsm9ds1_write_reg(stmdev_ctx_t* ctx, uint8_t reg, uint8_t* data, uint16_t len) {
+int32_t lsm9ds1_write_reg(stmdev_ctx_t* ctx, uint8_t reg, uint8_t* data, uint16_t len) {
     esp_err_t ret = ctx->write_reg(ctx->handle, reg, data, len);
     ESP_RETURN_ON_FALSE(ret == ESP_OK, ret, TAG, "write reg failed");
     return ESP_OK;
